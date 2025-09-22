469e07f2e456c75db6d587c9e500ae2e4492ebb5
KRIPT <bekmuhantalgat@gmail.com>
2025-09-22 13:50:51 +0500

    feat: add main router and add fastapi app

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from hci-project!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.app import main_router
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(main_router)
