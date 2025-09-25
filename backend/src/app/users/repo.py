3b6d7a554e47bf53109b61ab3b04623e9d1dd315
KRIPT <bekmuhantalgat@gmail.com>
2025-09-25 19:34:14 +0500

    feat: added 2 new func get_user_by_id and get_all_active in app.users.services.services.py

--- backend/src/app/users/repo.py
+++ backend/src/app/users/repo.py
@@ -16,6 +16,9 @@ class UserRepo:
         res = await self.db.execute(select(User).where(User.id == user_id))
         return res.scalar_one_or_none()
 
+    async def get_all_active(self) -> list[User]:
+        res = await self.db.execute(select(User).where(User.is_active == True))
+        return res.scalars().all()
 
 def get_user_repo(db: AsyncSession = Depends(get_async_session)) -> UserRepo:
     return UserRepo(db)
