61c626c24b2e3c786060744d2fe20bb0a81996ef
KRIPT <bekmuhantalgat@gmail.com>
2025-09-29 23:21:11 +0500

    feat: add users endpoint

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from app.auth.api.v1.jwt_router import router as jwt_router
 from app.auth.api.v1.router import router as auth_router
+from app.users.api.v1.router import router as users_router
 
 main_router = APIRouter()
 main_router.include_router(auth_router)
 main_router.include_router(jwt_router)
+main_router.include_router(users_router)
 
 __all__ = ['main_router']
